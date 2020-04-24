67a86e74dd88d7e3c761bb87ece419bd0a8d6f31
Lucas Assis <lucas.assis_@hotmail.com>
2020-04-23 23:37:35 -0300

    jogo funcionando e estilo adicionado

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, View, Text, Button, TextInput} from 'react-native';
-var input
+import {SafeAreaView, StyleSheet, View, Text, TouchableHighlight, TextInput, Alert} from 'react-native';
+let input
 
-class app extends React.Component{
+class App extends React.Component{
   constructor(props){
     super(props)
     this.state = {
@@ -13,137 +13,91 @@ class app extends React.Component{
     }
   }
 
-  componentDidMount(){
-    if(this.state.start === true){
-      this.id = setInterval(() => {
-        if(this.state.time > 0){
-          this.setState({time: this.state.time - 1})
-      }},1000)
-    }    
+  startGame = () => {
+    this.setState({start: true, time: 10})
+    this.id = setInterval(() => {
+      if(this.state.time > 0){
+        this.setState({time: this.state.time - 1})
+    }},1000)
   }
 
-  componentDidUpdate(){
-    if (this.state.time === 0) {
-      this.setState({start: false})
-      //Falta finalizar
+  componentDidUpdate() {
+    if (this.state.time === 0 && this.state.start === true) {
+      clearInterval(this.id)
+      Alert.alert(`A quantidade de caracteres digitados foi: ${this.state.text.length}`)
+      this.setState({start: false, text: ""})
     }
   }
 
   render(){
       return (
-          <SafeAreaView>
-              <View>
-                  <Text>
-                    Jogo da Digitação
-                  </Text>
-                  <Text>
-                    {this.state.title}
-                  </Text>
-                  <Text>
-                    {this.state.time}
-                  </Text>
-                  <TextInput
-                  value= {input}
-                  onChange={()=>{
-                    this.setState((input)=>{this.state.text= input})
-                  }}/>
-                  <Button OnPress={()=>{
-                    this.setState({start: true, time: 10})
-                  }} title="Começar"/>
-              </View>
-          </SafeAreaView>
+        <View style={styles.container}>
+            <Text style={styles.title}>
+              {"\nJogo da Digitação\n"}
+            </Text>
+            <Text style={styles.text}>
+              {this.state.title + "\n"}
+            </Text>
+            <Text style={styles.time}>
+              Tempo: {this.state.time}
+              {"\n"}
+            </Text>
+            <TextInput style={styles.input}
+              editable={this.state.start}                  
+              placeholder= "Digite aqui"
+              value= {input}
+              onChangeText={ input =>{this.setState({text: input})}}
+            />
+            <TouchableHighlight style={styles.button} onPress={this.startGame}>
+              <Text> Começar </Text>
+            </TouchableHighlight>
+        </View>
   )}
 }
 
-export default app
-/*
-class Jogo extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        texto: '', 
-        tempo: 10, 
-        play: false
-      }
-      this.contador;
-    }
-    componentDidMount(){
-      this.contador = setInterval(() => {
-        this.incrementaContadador()},1000
-      );
-      this.setState({play: true});
-    }
-  
-    incrementaContadador(){
-        this.setState((()=>({tempo: state.tempo - 1})))
-      if(this.state.tempo === 0 ){
-        this.setState({play: false})
-        this.componentWillUnmount()
-      }
-    }
-   
-    componentWillUnmount(){
-      clearInterval(this.contador);
-      Alert.alert(`                   ------Parabéns------\n               A sua pontuação é ${this.state.texto.length}`)
-    }
-  
-    render(){
-        return (
-          <View>
-            <Text style={styles.sectionTitle} > </Text>
-            <Text style={styles.sectionDescription}>Tempo: {this.state.tempo}</Text>
-            <Contador
-            style={{
-              height: 100, 
-              borderColor: 'black', 
-              borderWidth: 2, 
-              margin: 5}}
-            numberOfLines = {4}
-            editable={this.state.play}
-            multiline = {true}
-            placeholder='Digite aqui'
-            onChangeText={(text) => this.setState({texto: text})}
-            value={this.state.texto}
-          />
-          <View style={{
-              height: 50, 
-              borderColor: '#bad1f7',
-              backgroundColor: '#2672ed', 
-              borderWidth: 2, 
-              margin: 10,
-              width:350,
-              }}>
-           <TouchableOpacity onPress={() => {
-                this.setState({
-                  texto: '', 
-                  tempo: 10, 
-                  play: false
-                })
-                this.contador
-            }}>
-              <Text style = {styles.sectionDescription}>
-                Clique aqui para recomeçar
-              </Text>
-           </TouchableOpacity>
-        </View> 
-          
-            
-          </View>
-        )
-    }
-    }
-  
-  const App = () => {
-    return (
-      <>
-        <SafeAreaView>
-          <Text />
-          <Text style={styles.sectionTitle}>Jogo da Digitação</Text>
-          <Text />
-          <Text style={styles.sectionDescription}>Digite o maior numero de caracteres em 10 segundos!</Text>
-          <Jogo />
-          <Text />
-        </SafeAreaView>
-      </>
-    );
-  };*/
+export default App
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 20,
+    backgroundColor: 'rgb(0, 0, 0)',
+    borderWidth: 10,
+    borderColor: 'rgb(25, 25, 25)'
+  },
+  title: {
+    flex: 2,
+    textAlign: "center",
+    fontFamily: "Blippo, fantasy",
+    fontWeight: "bold",
+    fontSize: 26,
+    color: 'rgb(0, 255, 0)'
+  },
+  text: {
+    flex: 2,
+    textAlign: "center",
+    fontFamily: "Blippo, fantasy",
+    fontWeight: "normal",
+    fontSize: 18,
+    color: 'rgb(0, 255, 0)'
+  },
+  time: {
+    flex: 2,
+    textAlign: "center",
+    fontFamily: "Blippo, fantasy",
+    fontWeight: "bold",
+    fontSize: 18,
+    color: 'rgb(0, 255, 0)'
+  },
+  input: {
+    flex: 1,
+    backgroundColor: 'rgb(100, 254, 100)'
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "rgb(0,255,0)",
+    padding: 10,
+    borderWidth: 10
+  }
+});
